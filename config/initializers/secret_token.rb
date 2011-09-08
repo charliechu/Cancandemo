641d796a821364ccb2d60f035b49cdd005d9d94b
charliechu <charliechu@techbang.com.tw>
2011-09-08 14:23:02 +0800

    First commit .gitignore ddatabase.yml.example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cancandemo::Application.config.secret_token = 'c57ff29ffd255479eccbb8f434bac6b54011759887d4e61d3e5623a9297592cf7e409928a4a9d9ae16ff8b86ecb06299d488a1c35080ba3732dea11ac87ba458'
